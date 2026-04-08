646b524f3279470b167de1bd6f5a6956a4d6f3d8
CRAZYSHIT <NAVGHAREGAURAV80@GMAIL.COM>
2026-04-08 14:49:26 +0530

    Update and rename index.html to index.php

---
+++ ninth/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome Page</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="box">
+    <h1>Welcome</h1>
+
+    <p>
+        <?php include("datetime.php");?> 
+    </p>
+</div>
+
+</body>
+</html>
